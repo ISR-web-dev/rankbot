cd7a2329f284526f2aef25c098b8510baf878d3c
ISR-web-dev <137366576+ISR-web-dev@users.noreply.github.com>
2024-04-17 15:50:11 +0500

    Add files via upload

---
+++ Free/messages.js
@@ -0,0 +1,27 @@
+const messagesData = [
+    {
+        title: "default",
+        message: `Hi [client_name],
+I hope you're happy with the [service]! If you have a moment, I'd really appreciate it if you could leave a review on the Following Link
+[link]
+Thanks,
+[author]`
+    },
+    {
+        title: "Past",
+        message: `Hi [client_name],
+I hope you Loved the [service]! If you have a moment, I'd really appreciate it if you could leave a review on the Following Link
+[link]
+Thanks,
+[author]`
+    },
+    {
+        title: "future",
+        message: `Hi [client_name],
+I hope you'll love the [service]! If you have a moment, I'd really appreciate it if you could leave a review on the Following Link
+[link]
+Thanks,
+[author]`
+    }
+    
+];
---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.free').addEventListener('click', () => {
+  window.open('https://isr-web-dev.github.io/rankbot/Free');
+});
+document.querySelector('.pro').addEventListener('click', () => {
+  window.open('https://forms.gle/WP2W71EF5qrE3Y1PA');
+});
